ab73ba3bd2b2d38c725e04bb0e790de86059ba5b
DZH <singine@live.cn>
2023-02-15 02:26:11 +0800

    2023.02.15 02:26:17

---
+++ 01_helloworld.js
@@ -0,0 +1,9 @@
+var s;
+s = 'Hello World!';
+var a = 1;
+var b = true;
+console.log(s, a, b);
+function sum(n1, n2) {
+    return n1 + n2;
+}
+sum(111, 222);
---
+++ 01_helloworld.ts
@@ -0,0 +1,15 @@
+let s:string;
+s = 'Hello World!'
+
+let a: number = 1;
+let b = true;
+
+console.log(s,a,b);
+
+
+
+function sum(n1:number,n2:number):number{
+    return n1 + n2
+}
+
+sum(111,222);
